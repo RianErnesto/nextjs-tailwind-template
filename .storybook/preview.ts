bc56c182fb5f05f17191ca8ac212efaf3ee5e854
Rian Ernesto <rianernesto9@gmail.com>
2023-12-30 00:53:56 -0300

    :tada: init: Initial configuration done

---
+++ .storybook/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from "@storybook/react";
+import "../src/app/globals.css"
+import "../src/app/react-slick.css"
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import "./react-slick.css"
+import Providers from './providers'
+import { cn } from '@/services/utils/className'
+import Favicon from '@/components/Favicon'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], display: "swap", variable: "--font-inter" })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +19,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <Favicon />
+      <body className={cn(inter.variable)}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
